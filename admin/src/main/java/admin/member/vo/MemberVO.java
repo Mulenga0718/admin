52164db116cb39169703f887b000fe8f6c755b1b
User <User@61.81.2.80>
2022-04-10 22:19:24 +0900

    admin페이지

---
+++ admin/src/main/java/admin/member/vo/MemberVO.java
@@ -0,0 +1,53 @@
+package admin.member.vo;
+
+public class MemberVO {
+	String mem_email;
+	String mem_pass;
+	String mem_name;
+	String mem_add;
+	String mem_tel;
+	String mem_date;
+	String mem_status;
+	public String getMem_email() {
+		return mem_email;
+	}
+	public void setMem_email(String mem_email) {
+		this.mem_email = mem_email;
+	}
+	public String getMem_pass() {
+		return mem_pass;
+	}
+	public void setMem_pass(String mem_pass) {
+		this.mem_pass = mem_pass;
+	}
+	public String getMem_name() {
+		return mem_name;
+	}
+	public void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+	public String getMem_add() {
+		return mem_add;
+	}
+	public void setMem_add(String mem_add) {
+		this.mem_add = mem_add;
+	}
+	public String getMem_tel() {
+		return mem_tel;
+	}
+	public void setMem_tel(String mem_tel) {
+		this.mem_tel = mem_tel;
+	}
+	public String getMem_date() {
+		return mem_date;
+	}
+	public void setMem_date(String mem_date) {
+		this.mem_date = mem_date;
+	}
+	public String getMem_status() {
+		return mem_status;
+	}
+	public void setMem_status(String mem_status) {
+		this.mem_status = mem_status;
+	}
+}
